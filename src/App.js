3782620f9f68824c384ccad772c9e2ed30475f26
koksoya <b.koksoya@gmail.com>
2020-05-22 19:45:03 -0400

    Added google fonts, fontawesome CDNs and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <React.Fragment>
+      <h3>hello from app</h3>
+    </React.Fragment>
   );
 }
 
