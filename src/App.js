e7fe99025430378ff494d2d07a731a0b6f82916c
koksoya <b.koksoya@gmail.com>
2020-05-25 23:13:56 -0400

    Created <Product> <Cart> <Navbar>, <Details>, <Product> <ProductList> components and added routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-  return (
-    <React.Fragment>
-      <h3>hello from app</h3>
-    </React.Fragment>
-  );
+import Navbar from './components/Navbar/Navbar'
+import ProductList from './components/ProductList/ProductList'
+import Default from './components/Default/Default'
+import Details from './components/Details/Details'
+import Cart from './components/Cart/Cart'
+
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ProductList/ProductList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Product from '../Product/Product'
+
+const ProductList = () => {
+    return (
+        <div>
+            <Product />
+        </div>
+    );
+};
+
+export default ProductList;
