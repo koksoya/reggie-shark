4ba6a3d603210b9a601207f3392a701f667db3fc
koksoya <b.koksoya@gmail.com>
2020-05-27 16:46:54 -0400

    Added <Title> and data.js

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Product from '../Product/Product'
+import Product from "../Product/Product";
+import Title from "../Title/Title";
+import { storeProducts } from "../../data";
 
-const ProductList = () => {
-    return (
-        <div>
-            <Product />
+const ProductList = props => {
+  const [products, setProducts] = useState(storeProducts);
+
+  return (
+    <React.Fragment>
+      <div className="py-5">
+        <div className="container">
+          <Title name="our" title="products" />
+          <div className="row"></div>
         </div>
-    );
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default ProductList;
---
+++ src/data.js
@@ -0,0 +1,56 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Mountain Socks",
+    img: "img/socks-1.png",
+    price: 14,
+    company: "SpoxSox",
+    info:
+      "Are you ready for the weekend and adventures? Mismatched mountains socks are a perfect companion for all exciting trips - one is with green forest 🌲 and the other with high mountains ⛰️ and creek 🌊."
+  },
+  {
+    id: 2,
+    title: "Koala Socks",
+    img: "img/socks-2.png",
+    price: 12,
+    company: "Great Socks World",
+    info:
+      "Colorful socks Great Socks World designed and manufactured in Poland for the best quality."
+  },
+  {
+    id: 3,
+    title: "Koala Socks",
+    img: "img/socks-3.png",
+    price: 12,
+    company: "Great Socks World",
+    info:
+      "Colorful socks Great Socks World designed and manufactured in Poland for the best quality."
+  },
+  {
+    id: 4,
+    title: "Koala Socks",
+    img: "img/socks-4.png",
+    price: 12,
+    company: "Great Socks World",
+    info:
+      "Colorful socks Great Socks World designed and manufactured in Poland for the best quality."
+  },
+  {
+    id: 5,
+    title: "Koala Socks",
+    img: "img/socks-5.png",
+    price: 12,
+    company: "Great Socks World",
+    info:
+      "Colorful socks Great Socks World designed and manufactured in Poland for the best quality."
+  },
+  {
+    id: 6,
+    title: "Koala Socks",
+    img: "img/socks-6.png",
+    price: 12,
+    company: "Great Socks World",
+    info:
+      "Colorful socks Great Socks World designed and manufactured in Poland for the best quality."
+  }
+];
