f227c468745dc563c7c22b1053242c23d1f506d6
koksoya <b.koksoya@gmail.com>
2020-06-02 23:04:38 -0400

    title changed

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -7,16 +7,11 @@ import { ProductContext } from "../../context/product-context";
 
 const Product = props => {
   const { id, title, img, price, inCart } = props.product;
-  const { handleDetail, addToCart, openModal } = useContext(
-    ProductContext
-  );
+  const { handleDetail, addToCart, openModal } = useContext(ProductContext);
   return (
     <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
       <div className="card">
-        <div
-          className="img-container p-5"
-          onClick={() => handleDetail(id)}
-        >
+        <div className="img-container p-5" onClick={() => handleDetail(id)}>
           <Link to="/details">
             <img src={img} alt="product" className="card-img-top" />
           </Link>
